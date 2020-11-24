f1f15957c03d129106ab78ee016ab110e8cf9860
ntltd <nicolas.taltaud@gmail.com>
2020-11-24 11:15:05 +0100

    feat: add no sleep

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo } from 'react'
+import NoSleep from './noSleep'
+
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const noSleep = useMemo(() => new NoSleep(), [])
+  useEffect(() => {
+    if (typeof document === 'undefined') {
+      return
+    }
+    document.addEventListener('click', function enableNoSleep() {
+      document.removeEventListener('click', enableNoSleep, false)
+      noSleep.enable()
+    }, false)
+  }, [noSleep])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Simple <code>NoSleep.js</code> &times; <code>React</code> example.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/richtr/NoSleep.js/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          NoSleep.js
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
