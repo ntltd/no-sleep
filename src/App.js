f7d9986b24bfa208e5eae9e43893e93d637e7fcb
ntltd <nicolas.taltaud@gmail.com>
2020-11-24 11:43:39 +0100

    feat: handle no-sleep with a click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react'
+import React, { useMemo, useState, useCallback } from 'react'
 import NoSleep from './noSleep'
 
 import logo from './logo.svg'
@@ -6,30 +6,36 @@ import './App.css'
 
 function App() {
   const noSleep = useMemo(() => new NoSleep(), [])
-  useEffect(() => {
-    if (typeof document === 'undefined') {
-      return
-    }
-    document.addEventListener('click', function enableNoSleep() {
-      document.removeEventListener('click', enableNoSleep, false)
-      noSleep.enable()
-    }, false)
+  const [enable, setEnable] = useState(false)
+  const enableNoSleep = useCallback(() => {
+    noSleep.enable()
+    setEnable(true)
+  }, [noSleep])
+  const disableNoSleep = useCallback(() => {
+    noSleep.disable()
+    setEnable(false)
   }, [noSleep])
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
+        <button
+          className={`App-button App-button--${enable ? 'enabled' : 'disabled'}`}
+          onClick={enable ? disableNoSleep : enableNoSleep}
+        >
+          {enable ? 'enabled' : 'disabled'}
+        </button>
         <p>
           Simple <code>NoSleep.js</code> &times; <code>React</code> example.
         </p>
         <a
           className="App-link"
-          href="https://github.com/richtr/NoSleep.js/"
+          href="https://github.com/richtr/NoSleep.js/blob/master/example/index.html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          NoSleep.js
+          NoSleep.js example
         </a>
       </header>
     </div>
